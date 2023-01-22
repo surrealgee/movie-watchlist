29b8af0b0adbad47b355cb987d29b317aec70e5d
Jorge Arias <94802496+surrealgee@users.noreply.github.com>
2023-01-22 10:34:59 -0400

    Clean up code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// Initial Data
+
 const searchBtn = document.querySelector("#search-btn");
 const searchField = document.querySelector("#search-field");
 const moviesContainer = document.querySelector("#movies-container");
-const watchListPage = document.querySelector("#watchlist");
+
 let myWatchlist;
 if (localStorage.length > 0 && document.body.id === "watchlist") {
     renderMyWatchlist();
@@ -9,23 +11,28 @@ if (localStorage.length > 0 && document.body.id === "watchlist") {
     myWatchlist = []
 }
 
-// searchField.value = "blade runner"
+// Event Listeners
 
 searchBtn.addEventListener("click", renderSearch);
+
+// Listens to input depending on the page. Either Add/Remove.
 moviesContainer.addEventListener("click", (e) => {
     if (e.target.dataset.add) {
         addToWatchlist(e.target.dataset.add)
     }
-    if (e.target.dataset.remove) {
+    else if (e.target.dataset.remove) {
         removeFromWatchlist(e.target.dataset.remove)
     }
-
 })
 
-function renderMyWatchlist() {
-        myWatchlist = JSON.parse(localStorage.getItem("myWatchlist"))
-        moviesContainer.innerHTML = printMovies(myWatchlist, true);
-    }
+// Functions
+
+// Async Functions
+
+async function renderSearch() {
+    const searchResults = await getMoviesData();
+    moviesContainer.innerHTML = printMovies(searchResults.results, false);
+}
 
 async function addToWatchlist(movieId) {
     const searchResult = await getMoviesData();
@@ -33,13 +40,25 @@ async function addToWatchlist(movieId) {
     addToLocalStorage(chosenMovie[0])
 }
 
+async function getMoviesData() {
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=3ca008549b6464301a16aa99a8cb8c93&query=${searchField.value}`)
+
+    const searchResults = await response.json();
+
+    return searchResults;
+}
+
+// Regular Functions
+
+function renderMyWatchlist() {
+    myWatchlist = JSON.parse(localStorage.getItem("myWatchlist"))
+    moviesContainer.innerHTML = printMovies(myWatchlist, true);
+}
+
 function removeFromWatchlist(movieId) {
-    console.log(movieId) 
-    console.log(myWatchlist) 
-    let index;
     for (let movie of myWatchlist) {
         if (movie.id == movieId) {
-            index = myWatchlist.indexOf(movie)
+            let index = myWatchlist.indexOf(movie)
             myWatchlist.splice(index, 1);
         }
     }
@@ -52,50 +71,30 @@ function addToLocalStorage(element) {
     localStorage.setItem("myWatchlist", JSON.stringify(myWatchlist));
 }
 
-async function renderSearch() {
-    const searchResults = await getMoviesData();
-    // console.log(searchResults.results)
-    moviesContainer.innerHTML = printMovies(searchResults.results, false);
-}
-
-async function getMoviesData() {
-    // const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=3ca008549b6464301a16aa99a8cb8c93&query=blade runner`)
-
-    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=3ca008549b6464301a16aa99a8cb8c93&query=${searchField.value}`)
-
-    const searchResults = await response.json();
-
-    return searchResults;
-}
-
 function printMovies(movies, fromWatchList) {
-    // console.log(movies);
     let moviesHtml = "";
 
+
     for (let movie of movies) {
-        moviesHtml += `<div class="movie" id="${movie.id}">
-            <div class="movie-poster-box">
-                <img class="movie-poster" src="${getPosterUrl(movie.poster_path)}" alt="poster">
+        moviesHtml += `
+            <div class="movie" id="${movie.id}">
+                <div class="movie-poster-box">
+                    <img class="movie-poster" src="${getPosterUrl(movie.poster_path)}" alt="poster">
+                </div>
+                <div class="movie-info-box">
+                    <h3 class="movie-title">${movie.title}<span class="movie-score"><img src="img/star.png" class="small-icon" alt="">${movie.vote_average.toFixed(1)}</span></h3>
+                    <p class="movie-duration">${movie.release_date.slice(0, 4)}</p>
+                    <p class="movie-category">${movie.genre_ids}</p>                    
+                    <button class="watchlist-btn" data-id=${movie.id} data-${fromWatchList ? "remove" : "add"}=${movie.id}><img src="img/${fromWatchList ? "minus.png" : "plus.png"}" class="small-icon" alt="">${fromWatchList ? "Remove" : "Watchlist"}</button>
+                    <p class="movie-description">${movie.overview}</p>
+                </div>
             </div>
-            <div class="movie-info-box">
-                <h3 class="movie-title">${movie.title}<span class="movie-score"><img src="img/star.png" class="small-icon" alt="">${movie.vote_average.toFixed(1)}</span></h3>
-                                    
-                <p class="movie-duration">${movie.release_date.slice(0, 4)}</p>
-                <p class="movie-category">${movie.genre_ids}</p>
-                <button class="watchlist-btn" data-id=${movie.id} data-${fromWatchList ? "remove" : "add"}=${movie.id}><img src="img/${fromWatchList ? "minus.png" : "plus.png"}" class="small-icon" alt="">${fromWatchList ? "Remove" : "Watchlist"}</button>
-                <p class="movie-description">${movie.overview}</p>
-            </div>
-        </div>`;
+        `; // Button info is rendered according to which page is loaded, either index or watchlist.
     }
 
-    moviesHtml = `<div class="data-box">${moviesHtml}</div>`
-
-    return moviesHtml;
+    return moviesHtml = `<div class="data-box">${moviesHtml}</div>`
 }
 
 function getPosterUrl(url) {
     return `https://www.themoviedb.org/t/p/w94_and_h141_bestv2${url}`
 }
-
-
-// renderSearch();
